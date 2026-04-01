30a1658cedb5fa893a3a7092f1228090c0621815
sturyczbd02 <ben.sturycz@gmail.com>
2026-03-31 22:17:43 -0500

    filament change done

--- src/components/filament-change/filament-change.component.ts
+++ src/components/filament-change/filament-change.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { PrinterService } from '../../services/printer/printer.service';
+import { OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { SocketService } from '../../services/socket/socket.service';
 
 @Component({
   selector: 'app-filament-change',
@@ -6,4 +11,73 @@ import { Component } from '@angular/core';
   styleUrls: ['./filament-change.component.scss'],
   standalone: false,
 })
-export class FilamentChangeComponent {}
+
+export class FilamentChangeComponent implements OnInit, OnDestroy {
+  stage: 'heating' | 'retracting' | 'swap' | 'loading' | 'done' = 'heating';
+
+  constructor(
+    private router: Router,
+    private printerService: PrinterService,
+    private socketService: SocketService,
+  ) {}
+
+  cancelFilamentChange() {
+    this.printerService.executeGCode('M104 S0');
+    this.router.navigate(['/main-screen']);
+  }
+
+  private subscriptions: Subscription = new Subscription();
+
+    currentNozzleTemp = 0;
+    targetNozzleTemp = 220;
+
+  ngOnInit() {
+    this.printerService.executeGCode(`M104 S${this.targetNozzleTemp}`);
+
+    this.subscriptions.add(
+        this.socketService.getPrinterStatusSubscribable().subscribe(status => {
+            if (status?.tools?.[0]?.current !== undefined) {
+            this.currentNozzleTemp = status.tools[0].current;
+
+                if (
+                    this.stage === 'heating' &&
+                    this.currentNozzleTemp >= this.targetNozzleTemp - 2
+                ) {
+                    this.startRetraction();
+                }
+                }
+            })
+        );
+    }
+
+    ngOnDestroy() {
+        this.subscriptions.unsubscribe();
+    }
+
+    startRetraction() {
+        this.stage = 'retracting';
+
+        this.printerService.executeGCode('G90');
+        this.printerService.executeGCode('G1 E-100 F300');
+
+        setTimeout(() => {
+            this.stage = 'swap';
+        }, 4000);
+    }
+
+    startLoading() {
+        this.stage = 'loading';
+
+        this.printerService.executeGCode('G90');
+        this.printerService.executeGCode('G1 E200 F300');
+
+        setTimeout(() => {
+            this.stage = 'done';
+        }, 7000);
+    }
+
+    finishFilamentChange() {
+    this.printerService.executeGCode('M104 S0');
+    this.router.navigate(['/main-screen']);
+    }
+}
